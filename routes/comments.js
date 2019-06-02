a8114b03b26be06570a3374256b18babc7ba950d
Dino Firmalino <dfirma86@yahoo.com>
2019-06-01 20:06:08 -0700

    add functionality for user to edit and update campground comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,33 @@ router.post('/', isLoggedIn, (req, res) => {
   })
 })
 
+// Comments (edit route)
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if (err) {
+      console.log(err)
+      res.redirect('back')
+    } else {
+      res.render('comments/edit', {
+        campground_id: req.params.id,
+        comment: foundComment,
+      })
+    }
+  })
+})
+
+// Comments (update)
+router.put('/:comment_id', (req, res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if (err) {
+      console.log(err)
+      res.redirect('back')
+    } else {
+      res.redirect('/campgrounds/' + req.params.id)
+    }
+  })
+})
+
 // Middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
